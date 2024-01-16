8b8674047ce17ead24b8b2e3f39efbbbedd3b958
Kostiantyn Hromovyi <ydydydydydyyd@gmail.com>
2024-01-16 18:41:33 +0200

    added composer autoload

---
+++ task1/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'test/task',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a09c86cb2d68268a5da729a45c018024b0714abd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'test/task' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a09c86cb2d68268a5da729a45c018024b0714abd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
